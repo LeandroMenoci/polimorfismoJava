16d4dac439d89c9bf01d0965e1f6491a35581d23
leandromenoci <menoci.leandro@gmail.com>
2022-09-21 18:32:20 -0300

    Polimorfismo

---
+++ src/EditorVideo.java
@@ -0,0 +1,6 @@
+public class EditorVideo extends Funcionario {
+     public double getBonificacao() {
+         System.out.println("Editor de video");
+         return super.getBonificacao() + 100;
+     }
+}
---
+++ src/TesteGerente.java
@@ -0,0 +1,18 @@
+public class TesteGerente {
+    public static void main(String[] args) {
+        Gerente g1 = new Gerente();
+        g1.setNome("Leanddro");
+        g1.setCpf("222222222-22");
+        g1.setSalario(5000.00);
+
+        System.out.println(g1.getNome());
+        System.out.println(g1.getCpf());
+        System.out.println(g1.getSalario());
+
+        g1.setSenha(222);
+        boolean autenticou = g1.autentica(222);
+        System.out.println(autenticou);
+
+         System.out.println(g1.getBonificacao());
+    }
+}
